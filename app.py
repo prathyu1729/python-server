cd14edd1243fae48691c30dd1290b22e9d5d282d
prathyu1729 <prathyu1729@gmail.com>
2025-08-01 03:09:58 +0530

    feat: Agentic Fix PR

--- app.py
+++ app.py
@@ -67,9 +67,10 @@ def trigger_error():
     global error_enabled
     
     if error_enabled:
+    undefined_variable = ""  # Initialize the variable
+    result = undefined_variable + "This should cause an error"
         # INTENTIONAL ERROR: This will cause a NameError because 'undefined_variable' is not defined
         # This is the error that can be fixed in a PR
-        result = undefined_variable + "This should cause an error"
         return jsonify({'result': result})
     else:
         return jsonify({
