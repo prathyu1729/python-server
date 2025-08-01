7e16ecec87faa0d1fc05909165e174aaf45e2c50
prathyu1729 <prathyu1729@gmail.com>
2025-08-01 10:21:05 +0530

    feat: Agentic Fix PR

--- app.py
+++ app.py
@@ -118,3 +118,4 @@ def internal_error(error):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True) 
+    result = "This should cause an error"
